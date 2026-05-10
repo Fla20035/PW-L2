045677694ce182aca4106a9c9491c1de006bfaf9
Condrea Flavius-Valentin <condreaflavius2003@gmail.com>
2026-05-11 01:31:09 +0300

    fixxfeat: rezolvare ceva security feature CORS si add POST

--- server/index.js
+++ server/index.js
@@ -1,9 +1,14 @@
+const Project = require('./models/Project');
 const express = require('express');
-const app = express();
-const PORT = 3000;
+const cors = require('cors'); // pt comunicare intre porturile de back si front (3000 si 5173)
 const mongoose = require('mongoose');
 
-//mongusu legat la mongu 
+const app = express();
+app.use(cors()); // comunicare intre porturile de back si front (3000 si 5173)
+app.use(express.json()); // comunicare intre back si front (pt a putea citi ce trimite frontul in body-ul requestului)
+const PORT = 3000;
+
+// mongusu legat la mongu 
 mongoose.connect('mongodb://127.0.0.1:27017/dashboard')
     .then(function() {
         console.log('Conectat la MongoDB!');
@@ -12,99 +17,46 @@ mongoose.connect('mongodb://127.0.0.1:27017/dashboard')
         console.error('Eroare conectare MongoDB:', err);
     });
 
-
-
 app.use(express.json());
 
-// Prima ruta: raspunde la GET /
+// raspunde la GET pe ruta "/"
 app.get('/', function(req, res) {
     res.json({
         message: 'Serverul functioneaza!'
     });
 });
 
-//GET /api/projects/:id - returneaza un proiect specific dupa ID
-app.get('/api/projects/:id', function(req, res) {
-    const id = parseInt(req.params.id);
-    const project = projects.find(p => p.id === id);
 
-    if (!project) {
-        return res.status(404).json({ error: 'Proiectul nu a fost găsit' });
+// POST /api/projects - adauga un proiect nou in MongoDB
+app.post('/api/projects', async function(req, res) { //
+    try { //
+        
+        const newProject = new Project({ //
+            title: req.body.title, //
+            tech: req.body.tech, //
+            done: req.body.done || false //
+        }); //
+
+        const saved = await newProject.save(); //
+        
+        // verificare + daca respecta schema
+        res.status(201).json(saved); //
+    } catch (err) { //
+        // cath err 
+        res.status(400).json({ error: err.message }); //
+    } //
+});
+
+
+app.get('/api/projects', async function(req, res) { // 
+    try {
+        
+        const projects = await Project.find();
+        res.json(projects);
+    } catch (err) {
+        res.status(500).json({ error: 'Eroare ' + err });
     }
-    
-    res.json(project);
 });
-
-// GET /api/stats - returneaza statistici despre proiecte
-app.get('/api/stats', function(req, res) {
-    const total = projects.length;
-    const done = projects.filter(p => p.done).length;
-    const pending = total - done;
-
-    res.json({ total, done, pending });
-});
-
-
-//POST /api/projects - adauga un nou proiect
-// POST /api/projects
-app.post('/api/projects', function(req, res) {
-    const newProject = {
-        id: projects.length + 1,
-        title: req.body.title,
-        tech: req.body.tech,
-        done: req.body.done || false
-    };
-    
-    projects.push(newProject);
-    res.status(201).json(newProject); // 201 - Created
-});
-
-// DELETE /api/projects/:id - sterge un proiect dupa ID
-app.delete('/api/projects/:id', function(req, res) {
-    console.log('Received DELETE request for project with ID:', req.params.id);
-    const id = parseInt(req.params.id);
-    const index = projects.findIndex(p => p.id === id);
-
-    if (index === -1) {
-        return res.status(404).json({ error: 'Proiectul nu a fost găsit' });
-    }
-
-    const deletedProject = projects.splice(index, 1);
-    res.json(deletedProject[0]);
-});
-
-const projects = [{
-        id: 1,
-        title: "Antena DIY 137MHz",
-        tech: "Radio, Signal Processing",
-        done: false
-    },
-    {
-        id: 2,
-        title: "2D Platformer Unity",
-        tech: "Unity, C#",
-        done: true
-    },
-    {
-        id: 3,
-        title: "Band Pass Filter",
-        tech: "Electronics, Op-Amps",
-        done: true
-    },
-    {
-        id: 4,
-        title: "API Meteo",
-        tech: "React, API",
-        done: true
-    },
-];
-// GET /api/projects - returneaza toate proiectele
-app.get('/api/projects', function(req, res) {
-    res.json(projects);
-});
-
-
-
 
 // Porneste serverul
 app.listen(PORT, function() {
